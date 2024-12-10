addd698aee651f6dced3aade97e245197ec2c010
Y Sakashita <73250095+sakashita44@users.noreply.github.com>
2024-12-11 04:19:21 +0900

    feat: 箱ひげ図の平均値、外れ値、個別データの表示設定を追加

---
+++ scripts/plot_settings.py
@@ -0,0 +1,17 @@
+# 箱ひげ図作成時の設定 (sns.boxplotの引数`**kwargs`に渡す辞書)
+BOX_PLOT_SETTINGS = {}
+
+# (外れ値を除いた)平均値を示す点の設定 (matplotlib.plotの引数`**kwargs`に渡す辞書)
+MEAN_PLOT_SETTINGS = {}
+
+# 外れ値を示す点の設定 (sns.boxplotの引数`flierprops`に渡す辞書)
+OUTLIER_PLOT_SETTINGS = {}
+
+# 箱ひげ図に追加するジッターの設定 (sns.swarmplotの引数`**kwargs`に渡す辞書)
+SWARM_PLOT_SETTINGS = {}
+
+# 線グラフの設定 (sns.lineplotの引数`**kwargs`に渡す辞書)
+LINE_PLOT_SETTINGS = {}
+
+# example
+# MEAN_PLOT_SETTINGS['color'] = 'black'
